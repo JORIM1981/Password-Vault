04ed47a02be624cd612cca71b7102a2f8f5bf027
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-05-22 15:07:29 +0300

    add run test to check if the object has been initialized correctly pass

--- user.py
+++ user.py
@@ -1,3 +1,23 @@
 import random
 import string
 import pyperclip
+
+class User:
+    """
+    Create User class that generates new instances of a user.
+
+    """
+    user_list = []
+
+    def __init__(self, username, password):
+        """
+        method that defines the properties of a user.
+        """
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instance into the user list
+        """
+        User.user_list.append(self)
--- user_test.py
+++ user_test.py
@@ -1,2 +1,22 @@
 import unittest
 from user import User
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('OwitiCharles','XyZ3thf1')
+
+    def test_init(self):
+        """
+        test case to check if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'OwitiCharles')
+        self.assertEqual(self.new_user.password,'XyZ3thf1')
+
+if __name__ == "__main__":
+    unittest.main()
